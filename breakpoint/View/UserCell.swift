0aa8f3b977a9c306bdf865a45fdc6b43b57af542
Raphaël Reiter <raphaelreiter@Raphaels-iMac.local>
2017-10-29 21:05:19 +0100

    first commit

--- breakpoint/View/UserCell.swift
+++ breakpoint/View/UserCell.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class UserCell: UITableViewCell {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    
+    var showing = false
+    
+    func configureCell(profileImage image: UIImage, email: String, isSelected: Bool) {
+        self.profileImage.image = image
+        self.emailLbl.text = email
+        
+        if isSelected {
+            self.checkImage.isHidden = false
+        } else {
+            self.checkImage.isHidden = true
+        }
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -17,8 +33,15 @@ class UserCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        if selected {
+            if showing == false {
+                checkImage.isHidden = false
+                showing = true
+            } else {
+                checkImage.isHidden = true
+                showing = false
+            }
+        }
     }
 
 }
